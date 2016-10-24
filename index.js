e2e2ef205319c9a41ab43fb0abff9004c8a82967
Matt Burman <matt@mattburman.com>
2016-10-24 13:11:46 +0100

    Hello world for Clarifai with example image

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Twit broken on 6.x node, using 4.4.7
 
 const Twit = require('twit');
+var Clarifai = require('clarifai');
+const EXAMPLE_IMAGE = 'http://www.newyorker.com/wp-content/uploads/2016/01/Borowitz-Donald-Trump-1200.jpg';
 
 const bot = new Twit({
 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -13,4 +15,15 @@ console.log(process.argv[2]);
 const stream = bot.stream('statuses/filter', { track: process.argv[2] });
 
 stream.on('tweet', tweet => console.log(tweet.text));
+
+var clarifai = new Clarifai.App(
+  process.env.CLARIFAI_CLIENT_ID,
+  process.env.CLARIFAI_CLIENT_SECRET
+);
+
+clarifai
+	.models
+	.predict(Clarifai.GENERAL_MODEL, EXAMPLE_IMAGE)
+	.then(thing => console.log(thing.data.outputs[0].data.concepts))
+	.catch(err => console.log('err: ', err));
 
