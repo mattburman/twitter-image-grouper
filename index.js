1d2c1745ba1aa58fbd046d3a394e36b2d3ec8e25
Matt Burman <matt@mattburman.com>
2016-10-25 23:01:54 +0100

    Download images to a directory based on the top tag from Clarifai

--- index.js
+++ index.js
@@ -3,6 +3,15 @@
 const Twit = require('twit');
 var Clarifai = require('clarifai');
 const EXAMPLE_IMAGE = 'http://www.newyorker.com/wp-content/uploads/2016/01/Borowitz-Donald-Trump-1200.jpg';
+const DEFAULT_DIR = 'img';
+const dir = (process.argv[3] || DEFAULT_DIR);
+const dlImg = require('image-downloader');
+const path = require('path');
+
+console.log(process.argv);
+console.log(dir);
+require('shelljs/global'); // import shelljs for mkdir, ls, etc
+if (ls().indexOf(dir) === -1) mkdir(dir);
 
 const bot = new Twit({
 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -13,14 +22,27 @@ const bot = new Twit({
 
 console.log(process.argv[2]);
 const stream = bot.stream('statuses/filter', { track: process.argv[2] });
+stream.on('error', err => console.log(err));
 
 stream.on('tweet', tweet => {
-	// console.log(tweet.entities.media);
-	if (!tweet.entities.media || !tweet.entities.media[0]) return; // no image in tweet
+	if (tweet.retweeted || !tweet.entities.media || !tweet.entities.media[0]) return; // no image in tweet
+
+	// console.log(tweet.entities.media[0].media_url_https);
 
 	predict(tweet.entities.media[0].media_url_https, (err, res) => {
-    //console.log(res.map(el => el.name));
-		console.log(res[0].name + ': ' + tweet.entities.media[0].media_url_https);
+		if (err) console.log(err);
+		const newDir = dir + '/' + res[0].name;
+		if (ls(dir).indexOf(res[0].name) === -1) mkdir(newDir);
+
+		dlImg({
+			url: tweet.entities.media[0].media_url_https,
+			dest: path.join(__dirname, newDir) + '/' + res[0].name + '-' + tweet.user.screen_name + '.jpg',
+			done: (err, filename, image) => {
+				if (err) console.log(err);
+
+				console.log('Image at: ' + filename);
+			}
+		});
 	});
 });
 
